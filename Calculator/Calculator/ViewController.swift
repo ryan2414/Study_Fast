8eca2644eee4716758e1187470c5b1b1701f069b
ryan2414 <jackpar@nate.com>
2023-08-29 10:31:52 +0900

    계산기 만들기

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -16,7 +16,7 @@ enum Operation {
 }
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var numberOutputLabel: UILabel!
     
     var displayNumber = ""
@@ -29,8 +29,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
     @IBAction func tapNumberButton(_ sender: UIButton) {
         guard let numberValue = sender.titleLabel?.text else { return }
         if self.displayNumber.count < 9 {
@@ -56,18 +56,60 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tapDevideButton(_ sender: UIButton) {
+        self.operation(.Deivde)
     }
     
     @IBAction func tapMultiplyButton(_ sender: UIButton) {
+        self.operation(.Multiply)
     }
     
     @IBAction func tapSubtractButton(_ sender: UIButton) {
+        self.operation(.Subtract)
     }
     
     @IBAction func tapAddButton(_ sender: UIButton) {
+        self.operation(.Add)
     }
     
     @IBAction func tapEqualButton(_ sender: UIButton) {
+        self.operation(self.currentOperation)
+    }
+    
+    func operation(_ operation: Operation) {
+        if self.currentOperation != .unknown {
+            if !self.displayNumber.isEmpty {
+                self.secondOperand = self.displayNumber
+                self.displayNumber = ""
+                
+                guard let firstOperand = Double(self.firstOperand) else { return }
+                guard let secondOperand = Double(self.secondOperand) else { return }
+                
+                switch self.currentOperation {
+                case .Add:
+                    self.result = "\(firstOperand + secondOperand)"
+                case .Subtract:
+                    self.result = "\(firstOperand - secondOperand)"
+                case .Multiply:
+                    self.result = "\(firstOperand * secondOperand)"
+                case .Deivde:
+                    self.result = "\(firstOperand / secondOperand)"
+                default:
+                    break;
+                }
+                
+                if let result = Double(self.result), result.truncatingRemainder(dividingBy: 1) == 0 {
+                    self.result = "\(Int(result))"
+                }
+                self.firstOperand = self.result
+                self.numberOutputLabel.text = self.result
+            }
+            
+            self.currentOperation = operation
+        } else {
+            self.firstOperand = self.displayNumber
+            self.currentOperation = operation
+            self.displayNumber = ""
+        }
     }
 }
 
